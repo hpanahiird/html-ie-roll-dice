3bd2a29bd5d780935af9c96dfcb22bf8048ea28f
Hossein Panahi <h.panahi.ird@gmail.com>
2018-12-03 06:27:00 +0330

    added id for elements. added a simple js code

---
+++ script.js
@@ -0,0 +1,8 @@
+window.alert("Welcome To Roll Dice Game :)");
+
+function s() {
+    console.log("lngng")
+    window.alert("new game!");
+    
+};
+document.getElementById("new_game").onclick=s;   
